9a9c42f31c00f054737b77d2b7a4371de2c8ca68
williamtanuwijaya <will.tw0807@gmail.com>
2023-05-27 14:28:58 +0700

    update

--- p1/UKM_Laravel/app/Http/Controllers/produk.php
+++ p1/UKM_Laravel/app/Http/Controllers/produk.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class produk extends Controller
 {
     public function index(){
-        return view('produlk.master');
+        return view('produk.master');
     }
 }
